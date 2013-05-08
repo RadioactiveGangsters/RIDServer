49e1f24364ce000c4f3f8caf04ef9799bc044fec
Bas Lehmann <B.Lehmann@live.nl>
2013-05-08 15:06:42 +0200

    Added Sensor class. Added Util class.

---
+++ Data/sensor.h
@@ -0,0 +1,15 @@
+/* 
+ * File:   sensor.h
+ * Author: Bas
+ *
+ */
+
+#ifndef SENSOR_H
+#define	SENSOR_H
+
+#include<stdlib.h>
+#include<stdio.h>
+#include<time.h>
+
+#endif	/* SENSOR_H */
+
---
+++ Util/util.c
@@ -0,0 +1,22 @@
+/* 
+ * File:   util.c
+ * Author: Bas
+ *
+ */
+
+#include "util.h"
+
+inline unsigned int numlen(const unsigned int t)
+{
+    // Beats including math.h, is also faster than log10
+    return t<10?1:
+           t<100?2:
+           t<1000?3:
+           t<10000?4:
+           t<100000?5:
+           t<1000000?6:
+           t<10000000?7:
+           t<100000000?8:
+           t<1000000000?9: 10;
+    // no int is larger than 10
+}
