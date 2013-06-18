19bfc9538aeecc3b7d95914838d0aeab41df151a
LittleOneB <B.Lehmann@live.nl>
2013-06-18 12:09:13 +0200

    Keiharde commit van Jaseen

--- Monitor/connection.c
+++ Monitor/connection.c
@@ -1,4 +1,5 @@
 #include "connection.h"
+#include <errno.h>
 
 int socklisten()
 {
@@ -12,9 +13,12 @@ int socklisten()
 
 
     server_sockfd = socket( AF_INET, SOCK_STREAM, 0 );
+    if (! server_sockfd){
+        Log(LOGL_ERROR, "error sockfd" );
+    }
     server_address.sin_family = AF_INET;
     server_address.sin_addr.s_addr = inet_addr( "127.0.0.1" );
-    server_address.sin_port = htons( 10000 );
+    server_address.sin_port = htons( 4444 );
 
     server_len = (socklen_t)sizeof( server_address );
 
@@ -34,25 +38,46 @@ int socklisten()
         Log(LOGL_SYSTEM_ACTIVITY,"server wait...\n" );
 
         client_len = (socklen_t)sizeof( client_address );
-        client_sockfd = accept( server_sockfd, ( struct sockaddr *)&client_address, &client_len );
-
+        if((client_sockfd = accept( server_sockfd, ( struct sockaddr *)&client_address, &client_len ))<1)
+        {
+            int x = errno;
+             Log(LOGL_ERROR, "client sockfd error : %d \n", x);
+        }
         Log(LOGL_CLIENT_ACTIVITY, "Client connected \n" );
-
+        
+        char*p=malloc(sizeof*p);
+        *p='5';
+        for (int i=0; i< 20; i++){
+            ssize_t x;
+        if((x=write(client_sockfd, p, sizeof(char)))!=sizeof(char))
+            Log(LOGL_ERROR, "string cannot send %d: %d",x, errno);
+        
+         Log(LOGL_CLIENT_ACTIVITY, "send char: %c \n", *p );
+        }
+    //     
         if( fork() == 0 )
         {
+            Log(LOGL_CLIENT_ACTIVITY, "in fork loop" );
             if( read( client_sockfd, &ch, 1 ) ) break; 
             Log(LOGL_DEBUG, "Client send = %c\n", ch );
+            //
+            printf("received: %c\n", ch);
+           
 
             ch++;
 
             if( sleep( 5 ) ) break;
 
             Log(LOGL_DEBUG, "Server send = %c\n", ch );
+            //if( write( client_sockfd, &ch, 1 ) ) continue;
             if( write( client_sockfd, &ch, 1 ) ) continue;
+            printf("send: %c", ch);
             (void)close( client_sockfd );
             return EXIT_SUCCESS;
         }
         else
+            Log(LOGL_DEBUG, "socket closed" );
+      //  printf("socket closed");
             (void)close( client_sockfd );
 
     }
