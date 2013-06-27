a5ae27cf602c365671312fd36b186fbee54703d3
B. Al <berend.al@hva.nl>
2013-06-27 12:35:20 +0000

    documented new network config

--- Monitor/connection.c
+++ Monitor/connection.c
@@ -49,52 +49,45 @@ int AcceptClients(void)
 	{
 		Log(LOGL_WARNING,LOGT_NETWORK, "Network configuration missing from %s, using defaults.\n",networkinipath());
 	}
-	// TODO: address config
-	const struct sockaddr_in server_address=
+	// continue using defaults
 	{
-		.sin_family=AF_INET,
-		.sin_port = htons(iniparser_getint(config,"network:port",4444)),
-		.sin_addr.s_addr = inet_addr(iniparser_getstring(config,"network:address","127.0.0.1")),
-		// padding
-		.sin_zero = {0},
-	};
-	
-	//specify kind of socket required.
-	server_sockfd = socket(AF_INET, SOCK_STREAM, 0);
-	// did not get file descriptor?
-	if (!server_sockfd)
-	{
-	    Log(LOGL_ERROR, LOGT_NETWORK, "Network unavailable.\n");
-	    return EXIT_FAILURE;
-	}
+		// TODO: address config
+		const struct sockaddr_in server_address=
+		{
+			.sin_family=AF_INET,
+			.sin_port = htons((uint16_t)iniparser_getint(config,"network:port",61014)),
+			.sin_addr.s_addr = inet_addr(iniparser_getstring(config,"network:address","127.0.0.1")),
+			// padding
+			.sin_zero = {0},
+		};
+		
+		//specify kind of socket required.
+		server_sockfd = socket(AF_INET, SOCK_STREAM, 0);
+		// did not get file descriptor?
+		if (!server_sockfd)
+		{
+		    Log(LOGL_ERROR, LOGT_NETWORK, "Network unavailable.\n");
+		    return EXIT_FAILURE;
+		}
 
-	if(bind(server_sockfd, (struct sockaddr*)&server_address, (socklen_t)sizeof(server_address)))
-	{
-		Log(LOGL_ERROR,LOGT_NETWORK,"Network Address in use.\n");
-	    return EXIT_FAILURE;
-	}
+		if(bind(server_sockfd, (struct sockaddr*)&server_address, (socklen_t)sizeof(server_address)))
+		{
+			Log(LOGL_ERROR,LOGT_NETWORK,"Network Address in use.\n");
+			return EXIT_FAILURE;
+		}
 
-	// we now accept connections
-	// TODO: expected clients config
-	if(listen(server_sockfd, 5))
-	{
-		Log(LOGL_ERROR,LOGT_NETWORK,"Cannot accept any incoming connections.\n");
-		return EXIT_FAILURE;
-	}
+		// we now accept connections
+		if(listen(server_sockfd, iniparser_getint(config,"network:clients",10)))
+		{
+			Log(LOGL_ERROR,LOGT_NETWORK,"Cannot accept any incoming connections.\n");
+			return EXIT_FAILURE;
+		}
 
-	/* why are we telling, and who are we telling, their child changed state? 
-	if(signal(SIGCHLD, SIG_IGN) == SIG_ERR)
-	{
-		Log(LOGL_BUG,LOGT_NETWORK,"sigchld wat\n");
-		return EXIT_FAILURE;
-	}
-	do we need this?
-	*/
-
-	{
-		int*server_ret = malloc(sizeof*server_ret);
-		*server_ret = server_sockfd;
-		pthread_create(&netthread,NULL,&socklisten,server_ret);
+		{
+			int*server_ret = malloc(sizeof*server_ret);
+			*server_ret = server_sockfd;
+			pthread_create(&netthread,NULL,&socklisten,server_ret);
+		}
 	}
 	#endif
 	return EXIT_SUCCESS;
